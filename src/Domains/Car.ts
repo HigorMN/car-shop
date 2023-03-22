4cdab69078026804a9343b922ba6153710b8fe9b
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-22 17:00:03 -0300

    dominio do carro

---
+++ src/Domains/Car.ts
@@ -0,0 +1,25 @@
+import ICar from '../Interfaces/ICar';
+
+class Car {
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean;
+  protected buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+  protected id?: string;
+
+  constructor(car: ICar) {
+    this.model = car.model;
+    this.year = car.year;
+    this.color = car.color;
+    this.status = car.status || false;
+    this.buyValue = car.buyValue;
+    this.doorsQty = car.doorsQty;
+    this.seatsQty = car.seatsQty;
+    this.id = car.id;
+  }
+}
+
+export default Car;
