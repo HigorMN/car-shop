6574692c0ae19ceef1f126efa4799d561d1ece47
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-22 19:22:46 -0300

    feat: rota getAll e getById

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,5 +1,6 @@
 import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
+import IResponse from '../Interfaces/IResponse';
 import CarODM from '../Models/CarODM';
 
 class CarService {
@@ -21,6 +22,22 @@ class CarService {
     const newCar = await this.carODM.create(car);
 
     return this.createCarDomain(newCar);
+  }
+
+  public async getAll(): Promise<(Car | null)[] | undefined> {
+    const cars = await this.carODM.findAll();
+    const carsDomains = cars?.map((e) => this.createCarDomain(e));
+    return carsDomains;
+  }
+
+  public async getById(id: string): Promise<IResponse> {
+    try {
+      const car = await this.carODM.findById(id);
+      if (!car) return { status: 404, message: 'Car not found' };
+      return { car: this.createCarDomain(car) };
+    } catch (error) {
+      return { status: 422, message: 'Invalid mongo id' };
+    }
   }
 }
 
