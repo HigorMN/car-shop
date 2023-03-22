22032f6755e27a0d3ae6339d0f7fc8524980f32f
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-22 15:08:56 -0300

    feat: adicionando Middleware de erro

---
+++ src/Middlewares/ErrorHandler.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+class ErrorHandler {
+  public static handle(error: Error, _req: Request, res: Response, next: NextFunction) {
+    res.status(500).json({ message: error.message });
+    next();
+  }
+}
+
+export default ErrorHandler;
