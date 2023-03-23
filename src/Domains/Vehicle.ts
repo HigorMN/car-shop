8bd0b1fa9a0c20e992d587e163d5d50a47b72df2
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-23 16:22:58 -0300

    refactor: Implementando o dominio da class Vehicle e adicionando o dominio Motorcycle

---
+++ src/Domains/Vehicle.ts
@@ -0,0 +1,21 @@
+import IVehicle from '../Interfaces/IVehicle';
+
+class Vehicle {
+  protected id?: string;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean;
+  protected buyValue: number;
+
+  constructor(car: IVehicle) {
+    this.id = car.id;
+    this.model = car.model;
+    this.year = car.year;
+    this.color = car.color;
+    this.status = car.status || false;
+    this.buyValue = car.buyValue;
+  }
+}
+
+export default Vehicle;
