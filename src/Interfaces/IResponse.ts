3b3f5be6b255cf3e058ffe99089da4516f750ac5
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-23 13:32:20 -0300

    refactor: IResponse

--- src/Interfaces/IResponse.ts
+++ src/Interfaces/IResponse.ts
@@ -1,9 +1,6 @@
-import Car from '../Domains/Car';
-
-class IResponse {
-  status?: number;
-  message?: string;
-  car?: Car | null;
+interface IResponse {
+  status: number;
+  message: unknown;
 }
 
 export default IResponse;
